305119d9c3e06bd31cda09ee868185b0ba4680fc
MAHESH RUDRAPPA VAKKUND <maheshvakkund3107@Outlook.com>
2024-10-26 23:26:06 +0530

    Send Multiple events to multiple partitions.

--- kafka-basics/src/main/java/io/demo/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/demo/kafka/ProducerDemo.java
@@ -1,9 +1,6 @@
 package io.demo.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,31 +17,38 @@ public class ProducerDemo {
         properties.setProperty("bootstrap.servers","localhost:9092");
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty("batch.size", "400");
 
         //Create the producer
         KafkaProducer<String,String> producer =  new KafkaProducer<>(properties);
-
-        //Create a producer record.
-        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","Hello World");
-
-        //Send the data
-        producer.send(producerRecord, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                //Executes every time a record successfully sent or an exception is thrown.
-                if (e == null) {
-                    //the record was successfully sent
-                    logger.info("Recieved new metadata \n" +
-                            "Topic: " + recordMetadata.topic() + "\n" +
-                            "Partition: " + recordMetadata.partition() + "\n" +
-                            "Offset: " + recordMetadata.offset() + "\n" +
-                            "Timestamp: " + recordMetadata.timestamp());
-                }else{
-                    logger.error("Error while producing ", e);
-                }
+        for (int j = 0; j <= 10; j++) {
+            for (int i = 0; i <= 30; i++) {
+                //Create a producer record.
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello World " + i);
+                //Send the data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        //Executes every time a record successfully sent or an exception is thrown.
+                        if (e == null) {
+                            //the record was successfully sent
+                            logger.info("Recieved new metadata \n" +
+                                    "Topic: " + recordMetadata.topic() + "\n" +
+                                    "Partition: " + recordMetadata.partition() + "\n" +
+                                    "Offset: " + recordMetadata.offset() + "\n" +
+                                    "Timestamp: " + recordMetadata.timestamp());
+                        } else {
+                            logger.error("Error while producing ", e);
+                        }
+                    }
+                });
             }
-        });
-
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         //tell the producer to send all the data and block untill done -- synchronous
         producer.flush();
 
